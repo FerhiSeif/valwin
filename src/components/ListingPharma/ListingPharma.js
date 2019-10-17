712b6fc5ce40a95164b54d73414fd5845017e498
Anaïs Chevallier <achevallier@valwin.fr>
2019-10-17 12:09:12 +0200

    feat: pharmacy listing creation

--- src/components/ListingPharma/ListingPharma.js
+++ src/components/ListingPharma/ListingPharma.js
@@ -1,50 +1,85 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import phone from '../../icons/phone.svg';
 import website from '../../icons/website.svg';
-import devanture from '../../images/devanture.png';
+
 import './ListingPharma.css';
 
-const ListingPharma = (props) => {
+const ListingPharma = ({ pharmacies }) => {
   return (
-    <div className="ListingPharma-container">
-      <div className="ListingPharma-container-picture">
-        <img
-          className="ListingPharma-picture"
-          src={devanture}
-          alt="pharmacy-front"
-        />
-      </div>
-      <div className="ListingPharma-information">
-        <p className="ListingPharma-information-title">Normandie Pharma</p>
-        <p className="ListingPharma-information-text">
-          18 rue du chemin vert
-          <br />
-          14000 Caen
-        </p>
-        <div className="ListingPharma-information-links">
-          <div className="ListingPharma-information-links-phone">
+    <div>
+      {pharmacies.map(
+        ({ image, name, address, city, phoneNumber, websiteUrl, distance }) => (
+          <div className="ListingPharma-container">
             <img
-              className="ListingPharma-information-links-icons"
-              src={phone}
-              alt="phone"
+              className="ListingPharma-picture"
+              src={image}
+              alt="pharmacy-front"
             />
-            <a href={props.number}>&nbsp;&nbsp;Appeler |&nbsp;</a>
+            <div className="ListingPharma-information">
+              <p className="ListingPharma-information-title">{name}</p>
+              <p className="ListingPharma-information-text">
+                {address}
+                <br />
+                {city}
+              </p>
+              <div className="ListingPharma-information-links">
+                <a
+                  className="ListingPharma-information-links-phone"
+                  href={'tel:' + phoneNumber}
+                >
+                  <img
+                    className="ListingPharma-information-links-icons"
+                    src={phone}
+                    alt="phone"
+                  />
+                  <span className="ListingPharma-information-links-caption">
+                    Appeler
+                  </span>
+                </a>
+                <div className="ListingPharma-information-links-separator">
+                  |
+                </div>
+                <a
+                  className="ListingPharma-information-links-phone"
+                  href={websiteUrl}
+                >
+                  <img
+                    className="ListingPharma-information-links-icons"
+                    src={website}
+                    alt="website"
+                  />
+                  <span className="ListingPharma-information-links-caption">
+                    Site web
+                  </span>
+                </a>
+              </div>
+            </div>
+            <div className="ListingPharma-distance">
+              <p className="ListingPharma-distance-information">
+                à {distance} km
+              </p>
+            </div>
           </div>
-          <div className="ListingPharma-information-links-phone">
-            <img
-              className="ListingPharma-information-links-icons"
-              src={website}
-              alt="website"
-            />{' '}
-            <a href={props.website}>&nbsp;Site web</a>
-          </div>
-        </div>
-      </div>
-      <div className="ListingPharma-distance">
-        <p className="ListingPharma-distance-information">à 2km</p>
-      </div>
+        )
+      )}
     </div>
   );
+};
+
+ListingPharma.propTypes = {
+  pharmacies: PropTypes.arrayOf(
+    PropTypes.shape({
+      image: PropTypes.string,
+      name: PropTypes.string.isRequired,
+      address: PropTypes.string.isRequired,
+      city: PropTypes.string.isRequired,
+      phoneNumber: PropTypes.string,
+      websiteUrl: PropTypes.string,
+      distance: PropTypes.string.isRequired,
+    })
+  ),
 };
 
 export default ListingPharma;
